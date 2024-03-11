030e744faeb097f9fa1d028efaded1c940694815
luisjo819UCLA <luisjo819@g.ucla.edu>
2024-03-10 19:04:53 -0700

    Second commits

--- First.r
+++ First.r
@@ -3,47 +3,32 @@ library(data.table)
 library(survival) # For survival analysis, might need adaptations for Tobit models
 library(censReg)  # For Tobit regression
 library(dplyr)
+library(tidyverse)
+library(lubridate)  # For easy date handling
 
-# Set the working directory and load the data
-# setwd("path_to_your_data_folder")
-cd_f061_p01 <- fread("your_data_file.csv")  # Adjust the filename accordingly
+library(haven) # For reading Stata files
 
-# Rename and select variables
-cd_f061_p01 <- cd_f061_p01[, .(id = ieb_prs_id,
-                                firmid = betnr,
-                                birthyr = gebjahr,
-                                education = bild,
-                                dailywage = tentgelt,
-                                year = jahr,
-                                female = frau,
-                                federal_state = ao_bula,
-                                occupation_status_hrs = stib,
-                                emp_status = erwstat,
-                                begin_date = begorig,
-                                end_date = endorig,
-                                sic_codes = list(w73, w93, w03))]
+#Lets see all the dta files that are in the folder SIEED_7518_v1_test
+dta_files = list.files("SIEED_7518_v1_test", pattern = ".dta")
 
-# Applying filters and creating new variables as per the conditions in the SAS code
-cd_f061_p01 <- cd_f061_p01[firmid %in% c(".z", ".n", ".") & female == 0 & 
-                            between(federal_state, 1, 11) & begin_date < as.Date("2010-01-01") &
-                            dailywage >= 1 & !emp_status %in% c(109, 110, 202, 209, 210) &
-                            between(stib, 0, 4), ]
+#Lets read the dta files and store them in a list
+dta_list = lapply(dta_files, function(x) read_dta(paste0("SIEED_7518_v1_test/", x)))
+class(dta_list)
+length(dta_list)
 
-cd_f061_p01[, age := year - birthyr]
-cd_f061_p01 <- cd_f061_p01[age >= 20 & age <= 60]
+#Lets see the head of the dta_list
+for (i in 1:length(dta_list)){
+  print(dta_files[i])
+  print(head(dta_list[[i]],1))
+}
 
-# More transformations and calculations based on the SAS code...
-# (Due to the length and complexity, I need to take more time here.)
+for (i in 1:length(dta_list)){
+    names(dta_list[[i]]) %>%
+    str_detect("gebjahr") %>%
+    any() %>%
+    print()
+}
 
-# After all transformations, the final dataset might look something like this
-final_dataset <- cd_f061_p01  # This will have undergone all transformations, filters, etc.
-
-# Saving the final dataset
-fwrite(final_dataset, "final_dataset.csv")
-
-# For the Tobit models, I would typically prepare the data and then use censReg as follows:
-# Note: You'll need to adjust this according to your specific Tobit model requirements.
-tobit_model <- censReg(logwage ~ age + atbigfirm + other_variables, left = 0, right = Inf, data = your_data)
-summary(tobit_model)
-
-# Export the modified data, results, or summaries as needed
+#I think its using the last one!
+dta_list[[50]] %>%
+  head()
